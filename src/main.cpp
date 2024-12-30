63c576058a13a22462e211f83feaf8a3a6b41dae
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-12-29 23:31:35 -0500

    LOC reduc

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,10 @@ using namespace geode::prelude;
 class $modify(MyEditorUI, EditorUI) {
 	static void onModify(auto& self) { (void) self.setHookPriority("EditorUI::getCreateBtn", -2123456789); }
 	bool getBool(std::string_view key) { return Mod::get()->getSettingValue<bool>(key); }
-	int64_t getInt(std::string_view key) { return Mod::get()->getSettingValue<int64_t>(key); }
 	double getDouble(std::string_view key) { return Mod::get()->getSettingValue<double>(key); }
-	cocos2d::ccColor4B getColor(std::string_view key) { return Mod::get()->getSettingValue<cocos2d::ccColor4B>(key); }
-	CreateMenuItem* getCreateBtn(int id, int bg) {
-		auto result = EditorUI::getCreateBtn(id, bg);
-		if (!MyEditorUI::getBool("enabled")) return result;
-		ButtonSprite* buttonSprite = getBool("extraSafety") ? result->getChildByType<ButtonSprite>(0) : typeinfo_cast<ButtonSprite*>(result->getChildren()->objectAtIndex(0));
-		if (!buttonSprite) return result;
-		GameObject* gameObject = getBool("extraSafety") ? buttonSprite->getChildByType<GameObject>(0) : typeinfo_cast<GameObject*>(buttonSprite->getChildren()->objectAtIndex(1));
-		if (!gameObject || gameObject->m_objectID != id) return result;
+	CCLabelBMFont* makeLabel(int id) {
 		std::string fontFile = "bigFont.fnt";
-		int font = MyEditorUI::getInt("stackSizeFont");
+		int font = Mod::get()->getSettingValue<int64_t>("stackSizeFont");
 		if (font == -1) {
 			fontFile = "goldFont.fnt";
 		} else if (font == -2) {
@@ -26,8 +18,7 @@ class $modify(MyEditorUI, EditorUI) {
 		}
 		auto label = CCLabelBMFont::create(fmt::format("{}", id).c_str(), fontFile.c_str());
 		auto nodeID = fmt::format("{}/objectID-{}-stacksize", Mod::get()->getID(), id);
-		auto color = MyEditorUI::getColor("color");
-		auto padding = static_cast<float>(getDouble("padding"));
+		auto color = Mod::get()->getSettingValue<cocos2d::ccColor4B>("color");
 		if (MyEditorUI::getBool("readableMode") && font == -2)
 			label->setBlendFunc({GL_ONE_MINUS_DST_COLOR, GL_ONE_MINUS_SRC_ALPHA});
 		label->setAlignment(kCCTextAlignmentRight);
@@ -35,16 +26,34 @@ class $modify(MyEditorUI, EditorUI) {
 		label->setColor({color.r, color.g, color.b});
 		label->setOpacity(color.a);
 		label->setScale(static_cast<float>(getDouble("scale")));
-		label->setPosition(
-			{
-				buttonSprite->getPositionX() + buttonSprite->getContentWidth() / 2.f - padding,
-				buttonSprite->getPositionY() - buttonSprite->getContentHeight() / 2.f + padding
-			}
-		);
-		label->setZOrder(gameObject->getZOrder() + 2);
 		label->setID(nodeID);
-		buttonSprite->addChild(label);
-		buttonSprite->updateLayout();
+		return label;
+	}
+	CreateMenuItem* getCreateBtn(int id, int bg) {
+		auto result = EditorUI::getCreateBtn(id, bg);
+		if (!MyEditorUI::getBool("enabled")) return result;
+		CCLabelBMFont* label = makeLabel(id);
+		auto padding = static_cast<float>(getDouble("padding"));
+		if (getBool("extraSafety")) {
+			ButtonSprite* buttonSprite = result->getChildByType<ButtonSprite>(0);
+			if (!buttonSprite) return result;
+			GameObject* gameObject = buttonSprite->getChildByType<GameObject>(0);
+			if (!gameObject || gameObject->m_objectID != id) return result;
+			label->setPosition(
+				{
+					buttonSprite->getPositionX() + buttonSprite->getContentWidth() / 2.f - padding,
+					buttonSprite->getPositionY() - buttonSprite->getContentHeight() / 2.f + padding
+				}
+			);
+			label->setZOrder(gameObject->getZOrder() + 2);
+			buttonSprite->addChild(label);
+			buttonSprite->updateLayout();
+		} else {
+			label->setPosition({35 - padding, 2 + padding});
+			label->setZOrder(1);
+			result->addChild(label);
+			result->updateLayout();
+		}
 		return result;
 	}
 };
